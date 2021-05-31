aa558fcf13026e36c139f3dc0ea528b05d6a1691
Peter Pfrommer <>
2021-05-31 22:33:23 +0200

    even long csv files are now possible; changed regex for delimiter = ','

--- converterFunc.js
+++ converterFunc.js
@@ -20,18 +20,18 @@ function loadFile() {
 
     // Read file into memory as UTF-8   
     reader.readAsText(elem);
-         
+
     //file successfully read
     reader.onload = loadHandler;
 
     //file unsuccessfully read
-    reader.onerror = errorHandler; 
-    
+    reader.onerror = errorHandler;
+
 
 }
 
 //handles successful upload of file
-function loadHandler(event){
+function loadHandler(event) {
     var csv = event.target.result;
     //message when upload successful
     document.getElementById("uploadMessage").innerHTML = "Upload was successful!";
@@ -52,46 +52,46 @@ function writeToArray(csv) {
     //delimter is either " " or ","
     var delimiter = document.getElementById('delimInput').value;
 
-    
-    //for blank delimiter we need to split csv string at new line (check attached file)
-    if(delimiter == ' '){
-        var allData = csv.split(/\n/);  
+
+    //for blank delimiter we need to split csv string at new line (check png file attached in folder)
+    if (delimiter == ' ') {
+        var allData = csv.split(/\n/);
     }
-    //for comma delimiter we need to split csv string at carriage return or new line (check attached file)
-    if(delimiter == ','){
-        var allData = csv.split(/\r\n/)
+    //for comma delimiter we need to split csv string at carriage return or new line (check png file attached in folder)
+    if (delimiter == ',') {
+        var allData = csv.split(/\r|\n/)
     }
+
     var arrFinal = [];
-    for (var i=0; i<allData.length; i++) {
+    for (var i = 0; i < allData.length; i++) {
         //split at delimiter
-        //content := 1 data point 
         var content = allData[i].split(delimiter);
-            var arrTemp = [];
-                for (var j=0; j<content.length; j++) {
-                    //does not want to create another array, therefore solved without filter method
-                    //get rid of the NaNs 
-                    if(Number.isInteger(parseInt(content[j]))){
-                        //convert strings to ints & add to temporary array
-                        arrTemp.push(parseInt(content[j]));
-                    }
-                    else{
-                        break;
-                    }
-                }
+        //an array element "" could be a consequence
+        //therefore check content array for ""
+        if (content == "") {
+            break;
+        }
+        var arrTemp = [];
+        for (var j = 0; j < content.length; j++) {
+            //content := 1 data point 
+            //does not want to create another array, therefore solved without filter method
+            //get rid of the NaNs 
+            if (Number.isInteger(parseInt(content[j]))) {
+                //convert strings to ints & add to temporary array
+                arrTemp.push(parseInt(content[j]));
+            }
+            else {
+                break;
+            }
+        }
+
+
         //glue together final (outer) array
         arrFinal.push(arrTemp);
-
-        //TODO: get rid of arrays with length zero
-        // for (var j=0; j<arrFinal.length; j++) {
-        //     if(arrFinal[j].length == 0){
-        //         arrFinal.splice(1,j);
-        //     }
-        // }        
-
-
-
     }
-  
+
+
+
     console.log(allData);
     //console.log(arrTemp);
     //console.log(arrFinal[1]);
@@ -104,13 +104,8 @@ function writeToArray(csv) {
     //write to input box; change DOM 
     //TODO: how to format it with <br> ? 
     document.getElementById("inputFile").innerHTML = arrFinal;
-   
-    
-    // slice, split einsetzen: https://sebhastian.com/javascript-csv-to-array/
 
-    //escape tool: https://www.freeformatter.com/javascript-escape.html#ad-output
-
-
+    //call interpolation function
     interPolate(arrFinal);
 
 }
@@ -118,18 +113,16 @@ function writeToArray(csv) {
 
 
 
-function interPolate(arr){
+function interPolate(arr) {
+    //TODO: what interpolation should be used? 
 
-    //var arrNew = [...arr].map(x => x == 0 ? 9999 : x);
-
-    //what interpolation should be used? 
-
-    for (var i=0; i<arr.length; i++) {
-        for (var j=0; j<arr[j].length; j++) {
-            if(arr[i][j] == 0){
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[j].length; j++) {
+            if (arr[i][j] == 0) {
                 arr[i][j] = 9999;
             }
-            else{
+
+            else {
                 arr[i][j] = arr[i][j];
             }
         }
@@ -139,7 +132,7 @@ function interPolate(arr){
 
 
     console.log(arr);
-    
+
     //arr.join("<b/>");
 
     document.getElementById("outputFile").innerHTML = arr;
@@ -149,7 +142,14 @@ function interPolate(arr){
 
 
 
- //handles unsuccessful upload of file 
+//write back to csv input type:  1) same delimiter 2) same data (= same number of datapoints) 3) no zero values
+function writeToCsv() {
+
+}
+
+
+
+//handles unsuccessful upload of file 
 function errorHandler() {
     document.getElementById("uploadMessage").innerHTML = "Upload failed!";
 }
